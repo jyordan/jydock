0f30e741e2a6f086c7ce22daa2788725332ac0ab
Joseph Don Yordan <josephyordan@gmail.com>
2022-02-12 00:40:20 +0800

    WIP

--- clearredis.sh
+++ clearredis.sh
@@ -1,4 +1,4 @@
 cd "$(dirname "$0")"
 docker-compose stop redis
-sudo chown -R 1001:1001 "__volumes/redis/data/"
-rm -rf "__volumes/redis/data/*"
+rm -rf __volumes/redis/data/*
+chown -R 1001:1001 "__volumes/redis/data/"
