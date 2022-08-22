57691f3d1a9c355c48df8d22cbbadf04e53d9c2e
Joseph Don Yordan <josephyordan@gmail.com>
2022-08-23 01:17:18 +0800

    WIP

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,1 +1,4 @@
 SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+SET GLOBAL general_log = 1;
+SET GLOBAL general_log_file = "/var/log/mysql/general-log.log";
+FLUSH GENERAL LOGS;
