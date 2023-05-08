22d7dbbe7dc5b2f754a84307303cf5a75a55d0ec
Joseph Don Yordan <josephyordan@gmail.com>
2023-05-08 23:22:43 +0800

    Update

--- initweb.sh
+++ initweb.sh
@@ -2,4 +2,6 @@ cd "$(dirname "$0")"
 sh ./clearredis.sh
 docker-compose stop $(docker-compose ps --services | grep web)
 docker-compose up -d $1
-docker-compose exec $1 bash
+if [ -z "$2" ]; then
+  docker-compose exec $1 bash
+fi
