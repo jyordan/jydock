b262292220c7cb8391a391df34f3fb4ac7ef967d
Joseph Don Yordan <josephyordan@gmail.com>
2023-05-22 20:06:27 +0800

    Update

--- _web.sh
+++ _web.sh
@@ -1,7 +1,8 @@
 cd "$(dirname "$0")"
 sh ./_clear-redis.sh
 
-docker-compose stop $(docker-compose ps --services | grep web)
+docker-compose stop $(docker-compose ps --services | grep web
+docker-compose up -d $1
 
 if [ -n "$2" ]; then
   sh ./_bash.sh $1
