7377fbb441005e3cc8471a24b0154343605ece0c
Joseph Don Yordan <josephyordan@gmail.com>
2024-10-28 15:42:27 -0600

    WIP

--- _start.sh
+++ _start.sh
@@ -2,7 +2,7 @@ cd "$(dirname "$0")"
 # sh ./_clear-redis.sh
 
 docker-compose stop $1
-docker-compose up -d $1
+docker-compose up -d $1 --remove-orphans
 
 if [ -n "$2" ]; then
   sh ./_bash.sh $1
--- _web.sh
+++ _web.sh
@@ -2,7 +2,7 @@ cd "$(dirname "$0")"
 sh ./_clear-redis.sh
 
 docker-compose stop $(docker-compose ps --services | grep web)
-docker-compose up -d $1
+docker-compose up -d $1 --remove-orphans
 
 if [ -n "$2" ]; then
   sh ./_bash.sh $1
